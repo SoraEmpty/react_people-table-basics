8c3ca78a5f2ed3dcedd99fa1babaf8b04b5c4ec8
Dmytro Vaskiv <dimavaskiv9@gmail.com>
2025-11-06 18:58:14 +0200

    message

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,29 @@
+import { NavLink } from 'react-router-dom';
+
+export const NavBar = () => {
+  const activeClass = ( {isActive} : { isActive: boolean } ) => isActive ? "navbar-item has-background-grey-lighter" : "navbar-item";
+
+    return (
+              <nav
+      data-cy="nav"
+      className="navbar is-fixed-top has-shadow"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="container">
+        <div className="navbar-brand">
+          <NavLink className={activeClass} to="/">
+            Home
+          </NavLink>
+
+          <NavLink
+            className={activeClass}
+            to="/people"
+          >
+            People
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+    );
+}
---
+++ src/components/PeopleTable.tsx
@@ -0,0 +1,61 @@
+import { Person } from "../types";
+import { PersonLink } from "./PersonLink";
+
+interface Props {
+  people: Person[];
+  selectedSlug: string | undefined;
+  peopleByName: Map<string, Person>;
+}
+
+export const PeopleTable: React.FC<Props> = ({ people, selectedSlug, peopleByName }) => {
+  return (
+    <table
+      data-cy="peopleTable"
+      className="table is-striped is-hoverable is-narrow is-fullwidth"
+    >
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Sex</th>
+          <th>Born</th>
+          <th>Died</th>
+          <th>Mother</th>
+          <th>Father</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {people.map(person => {
+          const mother = person.motherName
+            ? peopleByName.get(person.motherName.toLowerCase().trim())
+            : undefined;
+
+          const father = person.fatherName
+            ? peopleByName.get(person.fatherName.toLowerCase().trim())
+            : undefined;
+
+          return (
+            <tr
+              data-cy="person"
+              key={person.slug}
+              className={person.slug === selectedSlug ? "has-background-warning" : ""}
+            >
+              <td><PersonLink person={person} /></td>
+              <td>{person.sex}</td>
+              <td>{person.born}</td>
+              <td>{person.died}</td>
+
+              <td>
+                {mother ? <PersonLink person={mother} /> : "-"}
+              </td>
+
+              <td>
+                {father ? <PersonLink person={father} /> : "-"}
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
